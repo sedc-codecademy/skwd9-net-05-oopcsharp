985989f1ed7058772012b8717ab9691ea1377cb4
risto-panchevski <risto.a.panchevski@snowsoftware.com>
2021-03-22 20:45:59 +0100

    Class 01 - First Console App

---
+++ G1/Solution01/Class01/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Class01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world!");
+
+            Console.ReadLine();
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("    *");
+            Console.WriteLine("   ***");
+            Console.WriteLine("  *****");
+            Console.WriteLine(" *******");
+            Console.WriteLine("*********");
+
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("    *");
+            Console.WriteLine("   ***");
+            Console.WriteLine("  * * *");
+            Console.WriteLine("    *");
+            Console.WriteLine("   * *");
+            Console.WriteLine("  *   *");
+        }
+    }
+}
