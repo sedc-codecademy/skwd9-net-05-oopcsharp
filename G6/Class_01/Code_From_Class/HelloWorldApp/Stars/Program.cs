efc9fc6a447d343f149046dc33cb8a1a227ce32e
Goce Kabov <Goce.Kabov@seavus.com>
2021-03-23 20:50:32 +0100

    added class 01 for G6

---
+++ G6/Class_01/Code_From_Class/HelloWorldApp/Stars/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Stars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Exercise 1 
+            //Console.BackgroundColor = ConsoleColor.White;
+            //Console.ForegroundColor = ConsoleColor.Green;
+            //Console.WriteLine("    *");
+            //Console.WriteLine("   ***");
+            //Console.WriteLine("  *****");
+            //Console.WriteLine(" *******");
+            //Console.WriteLine("*********");
+            //Console.ForegroundColor = ConsoleColor.DarkYellow;
+            //Console.WriteLine("    *");
+            //Console.WriteLine("    *");
+            //Console.WriteLine("    *");
+
+            // Exercise 2
+            Console.WriteLine("    *");
+            Console.WriteLine("    *");
+            Console.WriteLine("  * * *");
+            Console.WriteLine(" *  *  *");
+            Console.WriteLine("    *");
+            Console.WriteLine("  *   *");
+            Console.WriteLine("*       *");
+
+            Console.ReadLine();
+        }
+    }
+}
