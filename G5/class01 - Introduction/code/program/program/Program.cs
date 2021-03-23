03219cca6902a6b940762cced408fd4eb13208ea
viktorjakovlev <viktorjakovlev@hotmail.com>
2021-03-23 20:35:01 +0100

    class 01 exercise added

---
+++ G5/class01 - Introduction/code/program/program/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var argument in args) 
+            {
+                Console.WriteLine(argument);
+            };
+
+            //Console.WriteLine("Hello World!");
+            string consoleInput = Console.ReadLine();
+            Console.WriteLine(consoleInput);
+        }
+
+
+        
+    }
+}
