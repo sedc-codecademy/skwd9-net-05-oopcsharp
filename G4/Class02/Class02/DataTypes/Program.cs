817af6904db20134479929495ac7921642a4076d
kristina-s <spasevska.kristina87@gmail.com>
2021-03-24 21:42:26 +0100

    G4 class materials and homework added

---
+++ G4/Class02/Class02/DataTypes/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+
+namespace DataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello G4!");
+            #region DataTypes
+            // declaration
+            int integerVariable1 = 0;
+            // initialization
+            integerVariable1 = 5;
+            // declaration & initialization
+            int integerVariable2 = 15;
+
+            float floatVariable = 23.4F;
+            double doubleVariable = 84839674.257;
+
+            // strings have to be in double quotes
+            string name = "Kristina";
+            name = "Panche";
+            char operatorVariable = '*';
+
+            bool boolVariable = false;
+            long longerInteger = 7326525723758475843;
+
+            Console.WriteLine("---------- DataTypes -----------");
+            Console.WriteLine(integerVariable1);
+            Console.WriteLine(integerVariable2);
+            Console.WriteLine(floatVariable);
+            Console.WriteLine(doubleVariable);
+            Console.WriteLine(name);
+            Console.WriteLine(operatorVariable);
+            Console.WriteLine(boolVariable);
+            Console.WriteLine(longerInteger);
+            #endregion
+
+            #region Operators
+
+            Console.WriteLine("---------- Operators -----------");
+
+            // + operator sums two sides if they're numbers
+            int sumOfTwoNumbers = 7 + 8;
+            Console.WriteLine(sumOfTwoNumbers);
+            // + operator concatenates two strings
+            Console.WriteLine("5" + "4");
+
+            int modulusOperator = 10 % 3;
+            // it gives you an error if you try this 10 % "3"
+            Console.WriteLine(modulusOperator);
+
+            sumOfTwoNumbers += 10;
+            //sumOfTwoNumbers = sumOfTwoNumbers + 10
+            Console.WriteLine("Sum again: " + sumOfTwoNumbers);
+
+            Console.WriteLine(true || false); // true
+            Console.WriteLine(false ^ true); // true XOR
+
+            // asigning new values with logical operators
+            bool myBooleanVariable = true;
+            myBooleanVariable &= false;
+            // myBooleanVariable = myBooleanVariable && false
+            Console.WriteLine(myBooleanVariable); // false
+
+            int someNumber = 15;
+            //someNumber = "Pane"; //you cannot change the type, even with VAR
+            Console.WriteLine("===========================");
+            #endregion
+
+            #region Exercises
+            Console.WriteLine("---------- Exercises -----------");
+            // Exercise 01
+
+            double firstDouble = 445432.25;
+            double secondDouble = 65.28;
+            double divisionDouble = firstDouble / secondDouble;
+
+            int firstInteger = 345;
+            int secondInteger = 87;
+            int divisionInteger = firstInteger / secondInteger;
+
+            Console.WriteLine(divisionDouble);
+            Console.WriteLine(divisionInteger);
+            Console.WriteLine("===========================");
+
+            // Exercise 02
+
+            string bookString01 = "Zoki";
+            string bookString02 = "Vaksajns";
+            string sumBooksString = bookString01 + " " + bookString02;
+            Console.WriteLine(sumBooksString);
+
+            string numString01 = "9";
+            string numString02 = "3";
+            string sumNumString = numString01 + numString02;
+            // you can check the type of the variable with GetType() method
+            Console.WriteLine(sumNumString.GetType());
+
+            // Exercise 03
+            string randomString = "Random number";
+            int randomNumber = 42;
+            string resultRandom = randomString + randomNumber;
+            Console.WriteLine(resultRandom);
+
+            // Exercise 04
+            int phoneCredit = 102;
+            int priceMessage = 5;
+            int howManyMessages = phoneCredit / priceMessage;
+            Console.WriteLine("I can send " + howManyMessages + " messages.");
+            #endregion
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ G4/Class02/Homework.md
@@ -0,0 +1,39 @@
+# Class 2 Homework 📒
+## Task 1
+Create new console application “RealCalculator” that takes two numbers from the input and asks what operation would the user want to be done ( +, - , * , / ). Then it returns the result.
+* Test Data:
+  * Enter the First number: 10
+  * Enter the Second number: 15
+  * Enter the Operation: +
+* Expected Output:
+  * The result is: 25
+
+## Task 2
+Create new console application “AverageNumber” that takes four numbers as input to calculate and print the average.
+* Test Data:
+  * Enter the First number: 10
+  * Enter the Second number: 15
+  * Enter the third number: 20
+  * Enter the four number: 30
+* Expected Output:
+  * The average of 10, 15, 20 and 30 is: 18
+ 
+## Task 3:
+Create new console application “SwapNumbers”. Input 2 numbers from the console and then swap the values of the variables so that the first variable has the second value and the second variable the first value.
+Please find example below:
+* Test Data:
+  * Input the First Number: 5
+  * Input the Second Number: 8
+* Expected Output:
+  * After Swapping:
+  * First Number: 8
+  * Second Number: 5
+
+## Task 4:
+Write a program, where for a given number entered via the keyboard, it will print: 
+* "Tik" if divisible by 3, 
+* "Tak" if divisible by 5, 
+* "Tik - Tak" if divisible by 3 and 5. 
+* If the number is not divisible by 3 or 5, then print "Bad number".
+
+
