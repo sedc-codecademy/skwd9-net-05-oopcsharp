8cff3f6638bcf89bfc0dd52b78c7a0483f39bc7f
tstojanovska <stojanovskatanja03@gmail.com>
2021-03-29 21:16:04 +0200

    Class 03

---
+++ G2/Class03 - Loops and arrays/Code/Arrays/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //empty integer array with length 3
+            int[] emptyIntegerArray = new int[3];
+            emptyIntegerArray[0] = 5;
+            emptyIntegerArray[1] = 7;
+            emptyIntegerArray[2] = 33;
+
+            Console.WriteLine(emptyIntegerArray[1]);
+            // emptyIntegerArray[3] = 55; - error, out of index boundaries
+
+            string[] stringsArray = { "Bob", "Paul", "Jane" };
+            Console.WriteLine(stringsArray[1]);
+
+            Console.WriteLine($"StringsArray length: {stringsArray.Length}");
+
+           // double[] doublesArray = { 2, 33.3, 44.6, 7, true }; - mixed arrays are not allowed
+           double[] doublesArray = { 2, 33.3, 44.6, 7, 68 };
+            //doublesArray[5] = 55.9; - exception
+
+
+            //RESIZE
+            Array.Resize(ref doublesArray, 7);
+            doublesArray[5] = 55.9;
+
+            //INDEX OF
+            int indexFound = Array.IndexOf(doublesArray, 44.6);
+            Console.WriteLine("Index of 44.6:");
+            Console.WriteLine(indexFound);
+
+            int indexNotFound = Array.IndexOf(doublesArray, 5);
+            Console.WriteLine("Index of 5:");
+            Console.WriteLine(indexNotFound);
+
+            //REVERSE
+            Console.WriteLine("===Reverse===");
+            Console.WriteLine(doublesArray[0]);
+            Array.Reverse(doublesArray);
+            Console.WriteLine(doublesArray[0]);
+
+
+            string[] names = { "Bob", "Paul", "Jane" , "Marry", "Kate", "Sam"};
+            //iterates each member - similar to for-of in JS
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+                if (name == "Jane")
+                    break;
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ G2/Class03 - Loops and arrays/Code/Exercise/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter number of trees: ");
+            bool success = int.TryParse(Console.ReadLine(), out int trees);
+            if (success)
+            {
+                int n = 8;
+                int m = 5;
+                int treeApples = n * 12 * trees;
+                int result;
+                if (treeApples % m == 0)
+                {
+                    result = treeApples / m;
+                }
+                else
+                {
+                    result = treeApples / m + 1;
+                }
+                Console.WriteLine("You would need :" + result + " baskets!");
+            }
+            Console.ReadLine();
+        }
+    }
+}
