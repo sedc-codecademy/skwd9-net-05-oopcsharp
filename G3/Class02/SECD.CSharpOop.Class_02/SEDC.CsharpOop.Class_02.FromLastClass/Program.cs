10dbba341ef8832999809bfa9a9ad090006bc15c
Trajan Stevkovski <trajan.stevkovski@seavus.com>
2021-03-30 18:51:42 +0200

    Class03 part-1

---
+++ G3/Class02/SECD.CSharpOop.Class_02/SEDC.CsharpOop.Class_02.FromLastClass/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+
+namespace SEDC.CsharpOop.Class_02.FromLastClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region firstPart
+            //double a1 = 1;
+            //double b = 2;
+            //double c = 3;
+            //double d = 4;
+            //double nums = 4;
+            //double sum = (a1 + b + c + d) / 4;
+            //Console.WriteLine(sum);
+
+            Console.ReadLine();
+
+            string str = "3.7";
+
+            // this will pass fine
+            double parsedInt = double.Parse(str);
+            Console.WriteLine(parsedInt);
+
+            //string str2 = "Trajan";
+
+            // this will throw FormatException
+            //int parsedInt2 = int.Parse(str2);
+
+            //long bigNum = 123123123;
+            //string str3 = "123";
+
+            //int convertedBigNum = Convert.ToInt32(bigNum);
+            //long convertStringToLong = Convert.ToInt64(str3);
+
+            string str4 = "312";
+            bool isValidInput = int.TryParse(str4, out int parsedInteger);
+
+            Console.WriteLine(isValidInput);
+            if (isValidInput)
+            {
+                Console.WriteLine("Valid Parsing " + parsedInteger);
+                // do something...
+            }
+            else
+            {
+                Console.WriteLine("Not valid parsing");
+                // do something else...
+            }
+
+            string input = Console.ReadLine();
+            if(!int.TryParse(input, out int a))
+            {
+                Console.WriteLine("Please enter valid input!");
+            }
+            #endregion
+
+            // SWITCH
+
+            int input1 = 6;
+
+            //if(input1 == 1)
+            //{
+
+            //}else if(input1 == 2)
+            //{
+
+            //}
+
+            switch(input1)
+            {
+                case 1:
+                    // do something...
+                    Console.WriteLine(input1);
+                    break;
+                case 2:
+                    // do something...
+                    Console.WriteLine(input1);
+                    break;
+                case 3:
+                    // do something...
+                    Console.WriteLine(input1);
+                    break;
+                case 4:
+                    // do something...
+                    Console.WriteLine(input1);
+                    break;
+                case 5:
+                case 6:
+                    Console.WriteLine("We are the same " + input1);
+                    break;
+                default:
+                    Console.WriteLine("Not valid input SWITCH");
+                    break;
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ G3/Class03/SEDC.CSharpOop.Class03/SEDC.CSharp.Class03.Loops/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace SEDC.CSharp.Class03.Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region FOR
+            //for (int i = 0; i <= 10; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //for(int i = 0; i <= 100; i++)
+            //{
+            //    if(i % 2 != 0)
+            //    {
+            //        continue;
+            //    }
+
+            //    // -----
+            //    if(i == 78) // if we have 77 here it will never enter into this if clause
+            //    {
+            //        break;
+            //    }
+            //    // -----
+            //    Console.WriteLine("Is even " + i);
+            //    // 100 lines of code
+            //}
+            #endregion
+
+            #region WHILE
+
+            int counter = 0;
+            while(counter < 10)
+            {
+                Console.WriteLine(counter);
+                counter++;
+            }
+
+            bool isFourtyFour = false;
+            int counter1 = 0;
+            
+            while(!isFourtyFour)
+            {
+                if(counter1 == 44)
+                {
+                    isFourtyFour = true;
+                }
+
+                counter1 += 4;
+            }
+
+            Console.WriteLine(counter1 + " " + isFourtyFour);
+
+            #endregion
+
+
+            Console.ReadLine();
+        }
+    }
+}
