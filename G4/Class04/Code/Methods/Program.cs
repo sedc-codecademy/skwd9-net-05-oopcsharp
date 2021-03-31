9681363fd9f229555bffcacec5efb96668ac1ab1
kristina-s <spasevska.kristina87@gmail.com>
2021-03-31 21:31:40 +0200

    G4 class 4 added

---
+++ G4/Class04/Code/Methods/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+
+namespace Methods
+{
+    class Program
+    {
+        // Method with no parameters and no return
+        static void CheckDroid1()
+        {
+            Console.WriteLine("These aren't the droids you are looking for.");
+            Console.ReadLine();
+        }
+
+        // Method with parameters and no return
+        static void CheckDroid2(bool isJedi)
+        {
+            if (isJedi)
+            {
+                Console.WriteLine("These aren't the droids you are looking for.");
+            }
+            else
+            {
+                Console.WriteLine("Take the droids. It's fine.");
+            }
+            Console.ReadLine();
+        }
+
+        // Method with no parameters and return
+        static string JediMindTrick()
+        {
+            string statement = "Another mind trick that will confuse you all.";
+            return statement;
+            // return "Another mind trick that will confuse you all.";
+        }
+
+        // Method with parameters and return
+        static string CheckDroid3(bool isJedi)
+        {
+            //if (isJedi)
+            //{
+            //    return "These aren't the droids you are looking for.";
+            //}
+            //else
+            //{
+            //    return "Take the droids. It's fine.";
+            //}
+            // elegant way - please do this
+            return isJedi ? "These aren't the droids you are looking for." : "Take the droids. It's fine.";
+        }
+
+        static int Sum(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+        static int Subtract(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Methods!");
+
+            //CheckDroid1();
+
+            //CheckDroid2(false);
+
+            string resultFromJediMindTrick = JediMindTrick();
+            //Console.WriteLine(resultFromJediMindTrick);
+
+            string resultFromCheckDroid3 = CheckDroid3(true);
+            //Console.WriteLine(resultFromCheckDroid3);
+
+            // Exercise 01
+            Console.WriteLine("Enter an operation ( + or - ):");
+            string operation = Console.ReadLine();
+            if (operation != "+" && operation != "-")
+            {
+                Console.WriteLine("Sorry! Wrong input!");
+            }
+            else
+            {
+                Console.Write("Enter first number:");
+                int number1 = int.Parse(Console.ReadLine());
+                Console.Write("Enter second number:");
+                int number2 = int.Parse(Console.ReadLine());
+                if (operation == "+") Console.WriteLine(Sum(number1, number2));
+                if (operation == "-") Console.WriteLine(Subtract(number1, number2));
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ G4/Class04/Homework.md
@@ -0,0 +1,17 @@
+# Homework Class 4 📒
+
+## Task 1
+* Create a new method called FunWithStrings that will have one input parameter (string) and it will have no return
+* In that method you should display in console the following tasks for the string:
+  * print the reverse string
+  * print total number of vowels
+  * check if string is palindrome
+  * print the largest word
+  * print the smallest word
+  * print the count of words
+  * print the most used character (not space obviously)
+* Ask the user to enter a string and call the method with that string.
+
+## Task 2
+* Create a new method that will remove extra space characters
+* Call the method with the following text: The    best Lorem  Ipsum      Generator in all the sea!  Heave this  scurvy copyfiller fer yar            next  adventure  and cajol yar clients into walking  the plank with  ev'ry layout!  Configure         above, then get yer pirate ipsum...own the high seas,   argh!
