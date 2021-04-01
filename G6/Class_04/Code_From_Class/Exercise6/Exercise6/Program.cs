7eceb45d61cc90a7fc7bd4ee662534c0558887b8
Goce Kabov <Goce.Kabov@seavus.com>
2021-04-01 22:26:53 +0200

    added class 04

---
+++ G6/Class_04/Code_From_Class/Exercise6/Exercise6/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Exercise6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = new string[] { "John", "Bob", "Jane" };
+
+            while (true)
+            {
+                Console.Write("Please insert new name: ");
+                string userName = Console.ReadLine();
+                Array.Resize(ref names, names.Length + 1);
+                names[names.Length - 1] = userName;
+                Console.Write("Do you want to enter another name (Y/N): ");
+                string userAnswer = Console.ReadLine();
+
+                while (userAnswer.ToUpper() != "Y" && userAnswer.ToUpper() != "N")
+                {
+                    Console.WriteLine("You inserted invalid answer");
+                    Console.Write("We said Y or N: ");
+                    userAnswer = Console.ReadLine();
+                }
+                if (userAnswer.ToUpper() == "N")
+                {
+                    break;
+                }
+            }
+
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ G6/Class_04/Code_From_Class/Exercise6/Strings/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // string building
+            string fullName = "Jane Doe";
+            string age = "32";
+            //Concatanation
+            // string userInfo = fullName + " " + "is female";
+
+            // string formatting
+            // string userInfo = string.Format("{0} is female and she is {1} years old", fullName, age);
+
+            // interpolation
+
+            //string userInfo = $"{fullName} is female and she is {age} years old";
+            //Console.WriteLine(userInfo);
+
+            //percent
+            string stringWithPercent = string.Format("{0:P}", .33);
+            // Console.WriteLine(stringWithPercent);
+
+            //currency = may vary as it is connected to you system settings
+            string currency = string.Format("{0:C}", 150);
+            // Console.WriteLine(currency);
+
+            // phone numbers
+            string phoneNum = string.Format("{0:+389##-###-###}", 078555111);
+            //Console.WriteLine(phoneNum);
+
+            // escaping characters
+
+            // Console.WriteLine("Check your c:\\drive");
+            // Console.WriteLine("Vlatko Ilevski said: \"Lele lugje sto napravivme\"");
+            string extraText = "this should work";
+            //Console.WriteLine($@"Check your c:\drive, {extraText}");
+            //Console.WriteLine(@"Vlatko Ilevski said: ""Lele lugje sto napravivme""");
+
+            // string methods
+            string testString = "   We are learning C# and it is fun and easy. Okay maybe just fun. This is for test   ";
+            //Console.WriteLine(testString.ToUpper());
+            //Console.WriteLine(testString.ToLower());
+            //Console.WriteLine(testString.Length);
+            //Console.WriteLine(testString.Trim());
+            //Console.WriteLine(testString.Trim().ToUpper());
+
+            // StartsWith() => returns true or false
+            Console.WriteLine(testString.StartsWith("   We are "));
+
+            //indexOf => returns index (int)
+            //Console.WriteLine(testString.IndexOf("We"));
+            //Console.WriteLine(testString.IndexOf("Weasjkhflasfh"));
+
+            //split => returns array
+            //string[] splitedTestString = testString.Split(".");
+            //foreach (string text in splitedTestString)
+            //{
+            //    Console.WriteLine(text);
+            //}
+            //Console.WriteLine(splitedTestString.Length);
+            //Console.WriteLine(splitedTestString[0]);
+            //Console.WriteLine(splitedTestString[1]);
+
+            // substring => returns new string
+            string substring1 = testString.Substring(5);
+            // Console.WriteLine(substring1);
+            string substring2 = testString.Substring(6, 3);
+            Console.WriteLine(substring2);
+            Console.ReadLine();
+        }
+    }
+}
