e8c59dd84f1b7601ddfae6d8e2d261496d1a802e
tstojanovska <stojanovskatanja03@gmail.com>
2021-04-03 12:59:41 +0200

    Class 05

---
+++ G2/Class05 - Classes and objects/Code/Exercise1/Human.cs
@@ -0,0 +1,15 @@
+﻿
+
+namespace Exercise1
+{
+	public class Human
+	{
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public int Age { get; set; }
+		public string GetPersonStats()
+		{
+			return $"{FirstName} {LastName}, age: {Age}";
+		}
+	}
+}
---
+++ G2/Class05 - Classes and objects/Code/Exercise3/Program.cs
@@ -0,0 +1,46 @@
+﻿using Exercise3.Classes;
+using System;
+
+namespace Exercise3
+{
+    class Program
+    {
+        static Student FindStudent(string name, Student[] studentsArray)
+        {
+            foreach(Student student in studentsArray)
+            {
+                if(student.FirstName.ToLower() == name.ToLower())
+                {
+                    return student;
+                }
+            }
+            return null;
+        }
+		static void Main(string[] args)
+        {
+            Student[] students = new Student[]
+            {
+                new Student("Bojan", "G2", "Web Development"),
+                new Student("Jovana", "G2", "Web Design"),
+                new Student("Stefan", "G3", "QA"),
+                new Student("Bojana", "G5", "Networks"),
+                new Student("Aleksandar", "G3", "Web Design")
+            };
+
+            Console.WriteLine("Enter name to fina a student:");
+            string name = Console.ReadLine();
+
+            Student foundStudent = FindStudent(name, students);
+            if(foundStudent == null)
+            {
+                Console.WriteLine("The student was not found");
+            }
+            else
+            {
+                Console.WriteLine($"We found the student: {foundStudent.FirstName} {foundStudent.Academy}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
