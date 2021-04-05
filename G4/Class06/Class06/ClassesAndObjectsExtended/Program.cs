3cfb540984f9df428aaf5ccc61454b06285ccd91
kristina-s <spasevska.kristina87@gmail.com>
2021-04-05 21:10:33 +0200

    G4 class 06 added

---
+++ G4/Class06/Class06/ClassesAndObjectsExtended/Program.cs
@@ -0,0 +1,35 @@
+﻿using ClassesAndObjectsExtended.Models;
+using System;
+
+namespace ClassesAndObjectsExtended
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Person student01 = new Person();
+            student01.FirstName = "Bob";
+            student01.LastName = "Bobsky";
+            Console.WriteLine(student01.FirstName);
+
+            Person student02 = new Person("Blazo", "Blazov");
+            Console.WriteLine(student02.PersonInfo());
+
+            Person[] students = new Person[]
+            {
+                new Person("Jana", "Simjanovska"),
+                new Person("Leart", "Kamberi"),
+                new Person("Nikola", "Sheskokalov")
+            };
+
+            foreach (Person item in students)
+            {
+                Console.Write($"{item.FirstName} ");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
