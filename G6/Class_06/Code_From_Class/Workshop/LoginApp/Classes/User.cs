eeb6440b38f268380e5a4038b9403aafd45e62fd
Goce Kabov <Goce.Kabov@seavus.com>
2021-04-06 21:39:31 +0200

    added code from class

---
+++ G6/Class_06/Code_From_Class/Workshop/LoginApp/Classes/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LoginApp.Classes
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string[] Messages { get; set; }
+    }
+}
