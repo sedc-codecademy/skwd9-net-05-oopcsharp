0285f2cb43a802672e6748cc704503d042de13f7
Viktor Jakovlev <vicebad@hotmail.com>
2021-04-06 18:23:50 +0200

    class05 code finihed

---
+++ G5/class05 - ClassesAndObjects/code/Class05/Classes/MyClasses/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classes.MyClasses
+{
+    public class Product
+    {
+        public int Code { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public Person Person { get; set; }
+
+        public void Buy(double money) 
+        {
+            if (money >= Price)
+            {
+                Console.WriteLine($"You just bought {Name}!");
+            }
+            else 
+            {
+                Console.WriteLine($"You need {Price - money} to buy this product!");
+            }
+        }
+    }
+}
