7b61dadebe3478963f57f330532bcf3e34097d9b
Trajan Stevkovski <trajan.stevkovski@seavus.com>
2021-04-06 18:36:05 +0200

    Class06 part-1

---
+++ G3/Class06/SEDC.CSharpOop.Class06/SEDC.CSharpOop.Class06.ClassesApp/MyClasess/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SEDC.CSharpOop.Class06.ClassesApp.MyClasess
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        private long SSN { get; set; }
+
+        public void Talk(string text)
+        {
+            Console.WriteLine($"The human named {Name} is saying: {text}");
+        }
+    }
+}
---
+++ G3/Class06/SEDC.CSharpOop.Class06/SEDC.CSharpOop.Class06.ClassesApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using SEDC.CSharpOop.Class06.ClassesApp.MyClasess;
+using System;
+
+namespace SEDC.CSharpOop.Class06.ClassesApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.Name = "Trajan";
+            person.Age = 33;
+            // person.SSN = 1231231; // we cant access this becouse it is private;
+            person.Talk("Hello");
+
+            Console.WriteLine($"Hello my name is {person.Name} and im {person.Age} years old");
+
+
+            Console.ReadLine();
+        }
+    }
+}
