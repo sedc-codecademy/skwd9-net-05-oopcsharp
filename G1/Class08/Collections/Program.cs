5632a477720ed968ed94b35c1e505d1e270e4b2f
risto-panchevski <76558034+risto-panchevski@users.noreply.github.com>
2021-04-12 19:01:43 +0200

    Dictionary

---
+++ G1/Class08/Collections/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using Models;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1, Martin
+            // 2, Ivan
+            // 3, Goran
+            Dictionary<int, string> students = new Dictionary<int, string>
+            {
+                {1, "Martin" },
+                {2, "Ivan" },
+                {3, "Goran" },
+                {6, "Aleksandar" }
+            };
+
+            //Console.WriteLine("Please enter student card:");
+            //string cardString = Console.ReadLine();
+            //Console.WriteLine("Please enter student name:");
+            //string name = Console.ReadLine();
+
+            //if (!int.TryParse(cardString, out int card))
+            //{
+            //    Console.WriteLine("Wrong input.");
+            //    return;
+            //}
+
+            ////students.Add(card, name);
+            ////bool successfulAdd = students.TryAdd(card, name);
+
+            //bool keyExists = students.ContainsKey(card);
+
+            //if (!keyExists)
+            //{
+            //    students.Add(card, name);
+            //}
+
+            string student = students[6];
+            //students.Remove(6);
+
+            //students.Remove(6, out string student);
+
+            foreach (KeyValuePair<int, string> s in students)
+            {
+                Console.WriteLine($"{s.Key}: {s.Value}");
+            }
+
+            Dictionary<string, Person> citizens = new Dictionary<string, Person>();
+
+            citizens.Add("A000111", new Person("Risto", "Panchevski", new DateTime(1989, 7, 20), "Gjorce"));
+            citizens.Add("A999111", new Person("Adis", "Drndar", new DateTime(1999, 01, 01), "Gjorce"));
+
+            foreach (KeyValuePair<string, Person> citizen in citizens)
+            {
+                Console.WriteLine($"LK. {citizen.Key}, {citizen.Value.FullName}");
+            }
+        }
+    }
+}
---
+++ G1/Class08/Models/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Models
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName => $"{FirstName} {LastName}";
+        public DateTime DateOfBirth { get; set; }
+        public string Address { get; set; }
+
+        public Person(string firstName, string lastName, DateTime dateOfBirth, string address)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            DateOfBirth = dateOfBirth;
+            Address = address;
+        }
+    }
+}
