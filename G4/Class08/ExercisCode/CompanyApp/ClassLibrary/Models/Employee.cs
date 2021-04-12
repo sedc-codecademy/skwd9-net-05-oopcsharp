6ede5dd5e5625986d2b4ec17f0ea4d30783d1ddf
Manass2 <panemanass2@gmail.com>
2021-04-12 21:30:23 +0200

    exercise code

---
+++ G4/Class08/ExercisCode/CompanyApp/ClassLibrary/Models/Employee.cs
@@ -0,0 +1,30 @@
+﻿using ClassLibrary.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibrary.Models
+{
+    public class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public CompanyRole Role { get; set; }
+        protected double Salary { get; set; }
+
+        public Employee()
+        {
+
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine($"First Name: {FirstName}, Last Name: {LastName}, Salary: {Salary}");
+        }
+
+        public virtual double GetSalary()
+        {
+            return Salary;
+        }
+    }
+}
---
+++ G4/Class08/ExercisCode/CompanyApp/CompanyApp/Program.cs
@@ -0,0 +1,43 @@
+﻿using ClassLibrary.Models;
+using System;
+
+namespace CompanyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Employee[] company = new Employee[]
+            {
+                new Menager("Bob", "Bobski"),
+                new SalesPerson("John", "Dow"),
+                new SalesPerson("Rick", "Brown")
+            };
+
+            Menager bob = (Menager)company[0];
+            bob.AddBonus(5000);
+            Console.WriteLine(bob.GetSalary());
+            bob.PrintInfo();
+
+            SalesPerson john = (SalesPerson)company[1];
+            john.AddSuccessRevenue(5500);
+            Console.WriteLine(john.GetSalary());
+            john.GetSalary();
+            john.PrintInfo();
+
+
+
+
+
+
+
+
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
