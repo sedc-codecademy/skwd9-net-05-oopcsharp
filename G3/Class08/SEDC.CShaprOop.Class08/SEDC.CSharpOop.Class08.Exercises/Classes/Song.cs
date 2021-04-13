7ce024115dcb543f0df047f53e812ee9148e1e50
Trajan Stevkovski <trajan.stevkovski@seavus.com>
2021-04-13 20:33:58 +0200

    Class08 part-3

---
+++ G3/Class08/SEDC.CShaprOop.Class08/SEDC.CSharpOop.Class08.Exercises/Classes/Song.cs
@@ -0,0 +1,16 @@
+﻿namespace SEDC.CSharpOop.Class08.Exercises.Classes
+{
+    public class Song
+    {
+        public string Title { get; set; }
+        public Genre Genre { get; set; }
+        public int Length { get; set; }
+
+        public Song(string title, int length, Genre genre)
+        {
+            Title = title;
+            Length = length;
+            Genre = genre;
+        }
+    }
+}
