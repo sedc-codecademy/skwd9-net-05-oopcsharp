c0f05d4b088bd259fd55e7fc74ba0bc941e02471
Viktor Jakovlev <vicebad@hotmail.com>
2021-04-13 21:10:34 +0200

    class 08 finished

---
+++ G5/class08 - CollectionsAndLINQ/code/Exercises/ExerciseEntities/Models/Person.cs
@@ -0,0 +1,41 @@
+﻿using ExerciseEntities.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExerciseEntities.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public Genre FavoriteMusicType { get; set; }
+        public List<Song> FavoriteSongs { get; set; }
+
+        public Person(string firstName, string lastName, int age, Genre favoriteMusicType)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            FavoriteMusicType = favoriteMusicType;
+        }
+
+        public void GetFavSongs() 
+        {
+            if (FavoriteSongs.Count > 0)
+            {
+                foreach (var song in FavoriteSongs)
+                {
+                    Console.WriteLine($"{song.Title} - {song.Genre} - {song.Length}");
+                }
+            }
+            else 
+            {
+                Console.WriteLine($"{FirstName} {LastName} hates music!");
+            }
+        }
+
+    }
+}
---
+++ G5/class08 - CollectionsAndLINQ/code/Exercises/ExerciseOne/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExerciseOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> PhoneBook = new Dictionary<string, int>();
+            PhoneBook.Add("Viktor", 123456);
+            PhoneBook.Add("Angela", 321314);
+            PhoneBook.Add("Robert", 433245);
+            PhoneBook.Add("Marko", 324451);
+            PhoneBook.Add("Igor", 442312);
+
+            Console.Write("Enter name here: ");
+            string input = Console.ReadLine();
+
+            //PrintPhone1(PhoneBook, input);
+            PrintPhone2(PhoneBook, input);
+
+            Console.ReadLine();
+        }
+
+        public static void PrintPhone1(Dictionary<string, int> phoneBook, string name) 
+        {
+            if (phoneBook.ContainsKey(name))
+            {
+                Console.WriteLine($"{name}'s phone number is: {phoneBook[name]}");
+            }
+            else 
+            {
+                Console.WriteLine($"There is no {name} in this phonebook!");
+            }
+        }
+
+        public static void PrintPhone2(Dictionary<string, int> phoneBook, string name) 
+        {
+            foreach (var contactName in phoneBook)
+            {
+                if (contactName.Key == name) 
+                {
+                    Console.WriteLine($"{contactName.Key}'s phone number is: {contactName.Value}");
+                    return;
+                }
+            };
+
+            Console.WriteLine($"There is no {name} in this phonebook!");
+        }
+    }
+}
