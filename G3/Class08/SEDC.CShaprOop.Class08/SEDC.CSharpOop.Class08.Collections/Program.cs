0d33163bb62c237c04d8ae41267295c2e315fa07
Trajan Stevkovski <trajan.stevkovski@seavus.com>
2021-04-13 20:11:59 +0200

    Class08 part-2

--- G3/Class08/SEDC.CShaprOop.Class08/SEDC.CSharpOop.Class08.Collections/Program.cs
+++ G3/Class08/SEDC.CShaprOop.Class08/SEDC.CSharpOop.Class08.Collections/Program.cs
@@ -14,7 +14,7 @@ namespace SEDC.CSharpOop.Class08.Collections
             //Array.Resize(ref strArray, 10);
             //strArray[strArray.Length - 1] = ":::";
             // GoThroughCollection(strArray, "Array"); 
-            Console.WriteLine("Length: " + strArray.Length);// Get number of elements in the collection
+            //Console.WriteLine("Length: " + strArray.Length);// Get number of elements in the collection
 
             // ArrayList ( Accepts all types and does not have constraint on number of elements )
             ArrayList arrayList = new ArrayList() { "Hello", 2, true, 2.33 };
@@ -34,7 +34,7 @@ namespace SEDC.CSharpOop.Class08.Collections
             listOfInts.Add(11);
             listOfInts.Add(13);
             listOfInts.Add(15);
-            GoThroughCollection(listOfInts, "List"); // Go trough the whole collection with our function created above
+            // GoThroughCollection(listOfInts, "List"); // Go trough the whole collection with our function created above
 
             List<string> srtList = new List<string>() { "Hello", "World", "!" };
             srtList.Add("Trajan");
@@ -42,10 +42,63 @@ namespace SEDC.CSharpOop.Class08.Collections
             // srtList.AddRange(strArray);
             srtList.Remove("Trajan");
             srtList.Remove(strArray[1]);
-            Console.WriteLine(srtList[0]);
-            GoThroughCollection(srtList, "List");
-            Console.WriteLine("Length: " + srtList.Count); // Get number of elements in the collection
+            //Console.WriteLine(srtList[0]);
+            //GoThroughCollection(srtList, "List");
+            //Console.WriteLine("Length: " + srtList.Count); // Get number of elements in the collection
 
+            // type     key     value
+            Dictionary<string, string> dictionaryOne = new Dictionary<string, string>();
+            dictionaryOne.Add("song1", "Winds of Change");
+            dictionaryOne.Add("song2", "Enter Sandman");
+            // dictionaryOne.Add("song1", "asd"); // key are unique and we canot add 2 keys with same value
+            string song = dictionaryOne["song2"];
+            // Console.WriteLine(dictionaryOne[2]); // ditionary dont have indexes
+            //Console.WriteLine(song);
+            // GoThroughCollection(dictionaryOne, "Dictionary");
+            Dictionary<int, string> dictionaryTwo = new Dictionary<int, string>()
+            {
+                { 1, "Hello" },
+                { 2, "World" },
+                { 3, "!" },
+                { 4, "!" }
+            };
+            string item1 = dictionaryTwo[2];
+            //Console.WriteLine(item1);
+            dictionaryTwo.Remove(2);
+            // GoThroughCollection(dictionaryTwo, "Dictionary");
+            //int keyNumber = 3;
+            //Console.WriteLine(dictionaryTwo[keyNumber]);
+            //foreach(KeyValuePair<int, string> item in dictionaryTwo)
+            //{
+            //    Console.WriteLine(item.Key);
+            //    Console.WriteLine(item.Value);
+            //}
+            //Console.WriteLine("Length: " + dictionaryTwo.Count);
+
+
+            Queue<int> someQueue = new Queue<int>();
+            someQueue.Enqueue(25); // adds item in queue
+            someQueue.Enqueue(2);
+            someQueue.Enqueue(3);
+            someQueue.Enqueue(112);
+            //GoThroughCollection(someQueue, "Queue");
+            int someNum = someQueue.Dequeue();
+            //GoThroughCollection(someQueue, "Queue");
+            // someQueue[2]; // cannot use indexes
+            //Console.WriteLine(someQueue.Peek());
+            //Console.WriteLine("Length: " + someQueue.Count);
+
+            Stack<string> someStack = new Stack<string>();
+            someStack.Push("Hey"); // add item to the stack
+            someStack.Push("Hello");
+            someStack.Push("Hi");
+            someStack.Push("Zdravo");
+            //GoThroughCollection(someStack, "Stack");
+            //string someString = someStack.Pop();
+            //Console.WriteLine(someString);
+            //GoThroughCollection(someStack, "Stack");
+            //Console.WriteLine(someStack.Peek());
+            //Console.WriteLine("Length: " + someStack.Count);
 
             Console.ReadLine();
         }
