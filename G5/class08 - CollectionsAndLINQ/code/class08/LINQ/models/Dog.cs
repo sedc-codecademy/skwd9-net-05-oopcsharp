ef304bbecdc118ff2ccb5eee440e25502aa2f0f2
Viktor Jakovlev <vicebad@hotmail.com>
2021-04-15 18:55:17 +0200

    class 08 updated

---
+++ G5/class08 - CollectionsAndLINQ/code/class08/LINQ/models/Dog.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LINQ.models
+{
+    public class Dog
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
