853cb1a8c975f67e51cc66ce0da0c841c164b6b1
Damjan Stojanovski <damjan.stojanovski@haselt.com>
2021-04-15 21:09:54 +0200

    Exercise 1 added

---
+++ G3/Class09/Code/Classwork/Exceptions.Exercises/Exceptions.Exercises.Solutions/Genre.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exceptions.Exercises.Solutions
+{
+    public enum Genre
+    {
+        Comedy = 1,
+        Horror =2,
+        Action = 3,
+        Drama = 4,
+        SciFi =5
+    }
+}
---
+++ G3/Class09/Code/Classwork/Exceptions.Exercises/Exceptions.Exercises.Solutions/Movie.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exceptions.Exercises.Solutions
+{
+    public class Movie
+    {
+        public Movie(string title, Genre movieGenre, short rating)
+        {
+            if(rating >= 1 && rating <= 5)
+            {
+                this.Rating = rating;
+            }
+            else
+            {
+                try
+                {
+                    throw new Exception("The rating you added was not valid");
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            this.Title = title;
+            this.MovieGenre = movieGenre;
+            this.TicketPrice = Rating * 5;
+        }
+        public string  Title { get; set; }
+        public Genre MovieGenre { get; set; }
+        public short Rating { get; set; }
+        public double TicketPrice { get; set; }
+    }
+}
