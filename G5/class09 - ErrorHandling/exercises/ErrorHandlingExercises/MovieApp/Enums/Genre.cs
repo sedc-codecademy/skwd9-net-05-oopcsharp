d9f9c05d44b40b86f081b75da4fc3818bf023568
Viktor Jakovlev <vicebad@hotmail.com>
2021-04-17 11:32:11 +0200

    exercise 1 updated

---
+++ G5/class09 - ErrorHandling/exercises/ErrorHandlingExercises/MovieApp/Enums/Genre.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MovieApp.Enums
+{
+    public enum Genre
+    {
+        Comedy,
+        Horror,
+        Action,
+        Drama,
+        SciFi
+    }
+}
